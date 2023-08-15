9a47768e0889ac3740157ba0db820689ef4998c3
theseyipeters <theseyipeters@gmail.com>
2023-08-15 17:56:58 +0100

    tune app

--- tune/src/components/main/results.js
+++ tune/src/components/main/results.js
@@ -1,34 +1,51 @@
-import React from 'react'
-import axios from 'axios'
-import './results.css'
-import './main.css'
-import RenderArtists from './RenderArtists'
-
-
+import React from 'react';
+import axios from 'axios';
+import './results.css';
+import './main.css';
+import RenderArtists from './RenderArtists';
 
 export default function Results(props) {
-
   const renderArtists = () => {
-    return props.results.map(artist => (
+    return props.results.artists.map((artist) => (
       <div key={artist.id}>
-          {artist.images.length ? <a href={artist.external_urls.spotify}><img width={'210px'} height={'210px'} src={artist.images[0].url} alt={artist.name} /></a> : <div className='no-image'></div> } 
-          <br />
-          {artist.name}
+        {artist.images.length ? (
+          <a href={artist.external_urls.spotify}>
+            <img
+              width={'105px'}
+              height={'105px'}
+              src={artist.images[0].url}
+              alt={artist.name}
+            />
+          </a>
+        ) : (
+          <div className='no-image'></div>
+        )}
+        <br />
+        {artist.name}
       </div>
-    ))
-  }
+    ));
+  };
+
+  const renderTracks = () => {
+    return props.results.tracks.map((track) => (
+      <div key={track.id}>
+        <div>{track.name}</div>
+      </div>
+    ));
+  };
 
   return (
     <div>
       <div className='results-container'>
         <div className='result'>
+          <h2>Artists</h2>
           {renderArtists()}
         </div>
-    
+        <div className='result'>
+          <h2>Tracks</h2>
+          {renderTracks()}
+        </div>
       </div>
-
-        
     </div>
-  )
+  );
 }
-
--- tune/src/components/main/search.js
+++ tune/src/components/main/search.js
@@ -1,72 +1,51 @@
-import React from 'react'
-import axios from 'axios'
-import { useState, useEffect } from 'react';
-import './main.css'
+import React, { useState } from 'react';
+import axios from 'axios';
+import './main.css';
 
 export default function Search(props) {
-
-  
-  const [searchInput, setSearchInput] = useState('')
-  const [artists, setArtists] = useState([])
-  const [tracks, setTracks] = useState([])
-
-  const token = window.localStorage.getItem("token")
-  
-
-
-
-  // const renderArtists = () => {
-  //   return artists.map(artist => (
-  //     <div key={artist.id}>
-  //         {artist.images.length ? <img width={'200px'} height={'200px'} src={artist.images[0].url} alt={artist.name} /> : <div>No Image</div>} 
-  //         {artist.name}
-  //     </div>
-  //   ))
-  // }
-
-
-
+  const [searchInput, setSearchInput] = useState('');
+  const [artists, setArtists] = useState([]);
+  const [tracks, setTracks] = useState([]);
+  const token = window.localStorage.getItem('token');
 
   const search = async (e) => {
-    e.preventDefault()
-    const {data} = await axios.get("https://api.spotify.com/v1/search", {
+    e.preventDefault();
+    const { data } = await axios.get('https://api.spotify.com/v1/search', {
       headers: {
-        Authorization: `Bearer ${token}`
+        Authorization: `Bearer ${token}`,
       },
       params: {
         q: searchInput,
-        type: "artist" 
-      }
-    })
+        type: 'artist,track', // Include both artist and track types in the search
+      },
+    });
 
-    console.log(data)
-    
-    props.setSearchArtists([...data.artists.items])
-    // props.setSearchTracks ([...data.tracks])
+    console.log(data);
 
-    
-   
-    
-  }
-    
-
+    props.setSearchArtists([...data.artists.items]);
+    props.setSearchTracks([...data.tracks.items]);
+  };
 
   return (
-    <div className='search-pane'>
-      <div className='arrows'>
-        <i className='fas fa-arrow-left'></i>
-        <i className='fas fa-arrow-right'></i>
+    <div className="search-pane">
+      <div className="arrows">
+        <i className="fas fa-arrow-left"></i>
+        <i className="fas fa-arrow-right"></i>
       </div>
-      <div className='search-container'>
-        <i className='fas fa-search'></i>
-        <form className='form-bar' onSubmit={search}>
-        <input  onChange={event => setSearchInput(event.target.value)} type='text' className='searchbar' placeholder='Search artist, albums and tracks...' />
-        <button type='submit' className='btn' onClick={search}>Search</button>
-        </form> 
+      <div className="search-container">
+        <i className="fas fa-search"></i>
+        <form className="form-bar" onSubmit={search}>
+          <input
+            onChange={(event) => setSearchInput(event.target.value)}
+            type="text"
+            className="searchbar"
+            placeholder="Search artist, albums and tracks..."
+          />
+          <button type="submit" className="btn" onClick={search}>
+            Search
+          </button>
+        </form>
       </div>
     </div>
-  )
+  );
 }
-
-
-
